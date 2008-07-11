9c60b0fbe1be5ff94dd07c9dc82a7bb724476748
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2008-07-11 12:43:50 +0000

    Lo, my son:  be Liberal in what you accept, and Conservative in what you give

--- diamond/diamond/config.py
+++ diamond/diamond/config.py
@@ -36,8 +36,12 @@ for dir in dirs:
         continue # bloody emacs
       handle = open(os.path.join(dir, file))
       newSchemata = [x.strip() for x in handle]
-      if len(newSchemata) != 2:
+      if len(newSchemata) < 2:
         debug.deprint("Warning: Found schema registration file \"" + file + "\", but file is improperly formatted - schema type not registered", 0)
+        continue
+      if not os.path.exists(newSchemata[1]):
+        debug.deprint("Warning: not a valid path: %s" % newSchemata[1], 0)
+        debug.deprint("schema type not registered")
         continue
       newSchemata[1] = os.path.expandvars(newSchemata[1])
       schemata[file] = tuple(newSchemata)
