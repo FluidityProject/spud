cbed697edda985898ed2de1669f3aad5da5bea59
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2008-08-15 11:03:02 +0000

    Handle exceptions in plugins a bit more gracefully

--- diamond/diamond/plugins.py
+++ diamond/diamond/plugins.py
@@ -2,6 +2,7 @@ import os
 import os.path
 import sys
 import thread
+import traceback
 
 import gtk.gdk
 
@@ -49,6 +50,16 @@ def configure_plugins(suffix):
 def cb_decorator(f):
   def wrapper(*args, **kwargs):
     gtk.gdk.threads_enter()
-    f(*args, **kwargs)
+
+    try:
+      f(*args, **kwargs)
+    except:
+      debug.deprint("Plugin raised an exception:", 0)
+      tb = traceback.format_exception(sys.exc_info()[0] ,sys.exc_info()[1], sys.exc_info()[2])
+      tb_msg = ""
+      for tbline in tb:
+        tb_msg += tbline
+      debug.deprint(tb_msg, 0)
+
     gtk.gdk.threads_leave()
   return wrapper
