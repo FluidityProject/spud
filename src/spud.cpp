6e8cd53cfb6fc0ab4e697fecb63cbe4975131f1c
James R. Maddison <j.r.maddison@ed.ac.uk>
2010-03-12 14:54:19 +0000

    Adding nan and inf input support

--- src/spud.cpp
+++ src/spud.cpp
@@ -1531,8 +1531,20 @@ namespace Spud{
 
         vector<double> val;
         val.resize(tokens.size());
-        for(size_t i = 0;i<tokens.size();i++)
-          istringstream(tokens[i]) >> val[i];
+        for(size_t i = 0;i<tokens.size();i++){
+          for(size_t j = 0;j < tokens[i].size();j++){
+            tokens[i][j] = tolower(tokens[i][j]);
+          }
+          if(tokens[i] == "nan"){
+            val[i] = numeric_limits<double>::quiet_NaN();
+          }else if(tokens[i] == "inf"){
+            val[i] = numeric_limits<double>::infinity();
+          }else if(tokens[i] == "-inf"){
+            val[i] = -numeric_limits<double>::infinity();
+          }else{
+            istringstream(tokens[i]) >> val[i];
+          }
+        }
 
         set_option(basename + "/__value", val, rank, shape);
         for(const TiXmlAttribute* att = celement->FirstAttribute();att;att = att->Next()){
@@ -1704,9 +1716,8 @@ namespace Spud{
     if(verbose)
       cout << "string OptionManager::Option::data_as_string(void) const\n";
 
-    numeric_limits< double > double_limits;
     ostringstream data_as_string;
-    data_as_string.precision(double_limits.digits10);
+    data_as_string.precision(numeric_limits< double >::digits10);
     switch(get_option_type()){
       case(SPUD_DOUBLE):
         for(unsigned int i = 0;i < data_double.size();i++){
