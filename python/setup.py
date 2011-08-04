c1617d09d59889ddaa19b2f835daa62c802648ab
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2011-08-04 22:30:53 +0100

    A grand game of whack-a-mole. This fixes the maverick build. We'll keep iterating until they all work ...

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 import os.path
 
-module1 = Extension('libspud', sources = ['libspud.c'], libraries=["spud"], library_dirs=[os.path.abspath("..")])
+module1 = Extension('libspud', sources = ['libspud.c'], libraries=["spud"], library_dirs=[os.path.abspath("..")], include_dirs=[os.path.abspath("../include")])
 
 setup (name = 'libspud',
        version = '1.1.3',
