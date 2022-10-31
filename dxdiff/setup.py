a928c21b047142d8968f084c68707a4972bb2c4b
Stephan Kramer <s.kramer@imperial.ac.uk>
2022-10-31 19:11:02 +0000

    Revert back to distutils

--- dxdiff/setup.py
+++ dxdiff/setup.py
@@ -1,10 +1,21 @@
-from setuptools import setup
+from distutils.core import setup
+import os
+import os.path
+import glob
 
-setup(name='dxdiff',
+try:
+  destdir = os.environ["DESTDIR"]
+except KeyError:
+  destdir = ""
+
+setup(
+      name='dxdiff',
       version='1.0',
-      description="An XML aware diff tool",
-      author="The ICOM team",
-      author_email="fraser.waters08@imperial.ac.uk",
-      url="http://amcg.ese.ic.ac.uk",
-      packages=['dxdiff'],
-      scripts=["dxdiff/dxdiff"])
+      description="An XML aware diff tool.",
+      author = "The ICOM team",
+      author_email = "fraser.waters08@imperial.ac.uk",
+      url = "http://amcg.ese.ic.ac.uk",
+      packages = ['dxdiff'],
+      scripts=["dxdiff/dxdiff"],
+     )
+
