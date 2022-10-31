7aab038156cc0a50c3f7ea463cb2e54581367a0a
Stephan Kramer <s.kramer@imperial.ac.uk>
2022-10-31 11:51:18 +0000

    Use "pip install" instead of "python setup.py install"
    Add python3-pip as build dependency

--- python/setup.py
+++ python/setup.py
@@ -6,5 +6,12 @@ setup(name='libspud',
       description='Python bindings for libspud',
       ext_modules=[Extension('libspud', sources=['libspud.c'],
                              libraries=["spud"],
+                             # path to libspud (the actual library, not the c-extension python wrapper 
+                             # which will be put in the parent directory by "make libspud.la"
+                             # NOTE: For this to work you have to run "python setup.py build" from the
+                             # original directory of the current file separately before
+                             # running pip install, as pip will build the extension (if it's not built already)
+                             # outside of the current context
                              library_dirs=[abspath("..")],
+                             # path to spud.h
                              include_dirs=[abspath("../include")])])
