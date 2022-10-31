c38ea9a8df28d9088cbf64d302a092b2b2d0f817
Stephan Kramer <s.kramer@imperial.ac.uk>
2022-10-31 19:23:57 +0000

    Revert "Revert "Revert back to distutils""
    This reverts commit 3e17fb754a831de11a187ebdfffa6510e138c9ec.

--- python/setup.py
+++ python/setup.py
@@ -1,17 +1,9 @@
-from os.path import abspath
-from setuptools import setup, Extension
+from distutils.core import setup, Extension
+import os.path
 
-setup(name='libspud',
-      version='1.1.3',
-      description='Python bindings for libspud',
-      ext_modules=[Extension('libspud', sources=['libspud.c'],
-                             libraries=["spud"],
-                             # path to libspud (the actual library, not the c-extension python wrapper 
-                             # which will be put in the parent directory by "make libspud.la"
-                             # NOTE: For this to work you have to run "python setup.py build" from the
-                             # original directory of the current file separately before
-                             # running pip install, as pip will build the extension (if it's not built already)
-                             # outside of the current context
-                             library_dirs=[abspath("..")],
-                             # path to spud.h
-                             include_dirs=[abspath("../include")])])
+module1 = Extension('libspud', sources = ['libspud.c'], libraries=["spud"], library_dirs=[os.path.abspath("..")], include_dirs=[os.path.abspath("../include")])
+
+setup (name = 'libspud',
+       version = '1.1.3',
+       description = 'Python bindings for libspud',
+       ext_modules = [module1])
